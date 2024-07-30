e25cce30c51d6675cdd3b485077ac55606c5bcdd
AndriiKozlovskyi <andrii@software.com>
2024-07-31 01:33:26 +0200

    user service fixed, project service add

--- auth-service/src/main/java/com/safalifter/authservice/client/CustomErrorDecoder.java
+++ auth-service/src/main/java/com/safalifter/authservice/client/CustomErrorDecoder.java
@@ -14,7 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 public class CustomErrorDecoder {
 //    private final ObjectMapper mapper = new ObjectMapper();
-//
+
 //    @Override
 //    public Exception decode(String methodKey, Response response) {
 //        try (InputStream body = response.body().asInputStream()) {
